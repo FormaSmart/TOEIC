ee413860d8741dfde940aaba96a6438f25a33f21
Zo <zoe.marday@outlook.com>
2022-05-30 09:34:46 +0200

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,31 @@
-const items = document.querySelectorAll('.accordion button');
-
-function toggleAccordion() {
-    const itemToggle = this.getAttribute('aria-expanded');
-
-    for (i = 0; i < items.length; i++) {
-        items[i].setAttribute('aria-expanded', 'false');
-    }
-
-    if (itemToggle == 'false') {
-        this.setAttribute('aria-expanded', 'true');
-    }
+function setCookie(cookieName, cookieValue, numdaystilexpireasinteger) {
+    var d = new Date();
+    d.setTime(d.getTime() + (numdaystilexpireasinteger*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cookieName+ "=" + cookieValue + ";" + expires + ";path=/";
 }
 
-items.forEach((item) => item.addEventListener('click', toggleAccordion));
+function getCookie(cookieName) {
+    var name = cookieName+ "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
+function showLaw() {
+    var x = getCookie("cookieName");  //call cookie to get its value
+    if (x != "") {
+        $("#lawmsg").remove();
+    } else {
+        setCookie("cookieName", "cookieValue", 2);
+    }
+}
